cce42e663e5cae5b3e9c0c787c48dab95413c003
k-michaela <michaela.kerberova@gmail.com>
2025-01-18 13:58:23 +0100

    welcome text

--- engeto-project-2.py
+++ engeto-project-2.py
@@ -1,7 +1,44 @@
 """
-projekt_2.py: druhý projekt do Engeto Online Python Akademie
+engeto-project-2.py: druhý projekt do Engeto Online Python Akademie
 
 author: Michaela Jochcová (Kerberová)
 email: michaela.kerberova@gmail.com
 discord: k.michaela
 """
+
+import random
+
+# separator
+separator = "-" * 50
+
+
+# welcome text 
+print(f"""
+Hi there!
+{separator}
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+{separator}
+Enter a number: """
+)
+   
+
+
+
+
+# random digit generation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
