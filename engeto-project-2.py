052f65afb41bdb749078ef6db8813c6bf11bbbf0
k-michaela <michaela.kerberova@gmail.com>
2025-01-20 07:26:17 +0100

    function: play game and editing the entire code

--- engeto-project-2.py
+++ engeto-project-2.py
@@ -1,5 +1,5 @@
 """
-engeto-project-2.py: druhý projekt do Engeto Online Python Akademie
+engeto-project-2.py: druhý projekt do Engeto Online Python Akademie, Bulls&Cows
 
 author: Michaela Jochcová (Kerberová)
 email: michaela.kerberova@gmail.com
@@ -7,74 +7,82 @@ discord: k.michaela
 """
 
 import random
+import re
 
-# separator
+# separator definition
 separator = "-" * 50
 
-# welcome text 
-print(f"""
+# random four-digits number generator
+def generate_random_number():
+    while True:
+        number = ''.join(random.choices('0123456789', k=4))
+        if number[0] != '0' and len(set(number)) == 4:
+            return number
+
+# inserting player's input and checking number entered correctly
+def check_entered_number(input):
+    if not re.match(r'^[1-9]\d{3}$', input) or len(set(input)) != 4:
+        print("Please enter a valid four-digit number without duplicates and not starting with 0.", separator, sep="\n")
+        return False
+    return True
+
+# bulls and cows calculation
+def bulls_and_cows_count(generate_random_number, player_guess):
+    bulls = sum(1 for index, digit in enumerate(player_guess) if generate_random_number[index] == digit)
+    cows = sum(1 for digit in player_guess if digit in generate_random_number) - bulls
+    return bulls, cows
+
+# evaluation of number of attempts
+def evaluate_players_score(number_of_attempts):
+    evaluation_messages = {
+        range(1, 6): "Wow, excellent score!",
+        range(6, 11): "Very good score!",
+        range(11, 16): "Not so bad!",
+        range(16, 21): "Could be better!",
+        range(21, 1000): "Practice and you will definitely improve."
+    }
+    
+    for attempts_range, evaluation_message in evaluation_messages.items():
+        if number_of_attempts in attempts_range:
+            print(evaluation_message)
+            break
+
+# play game
+def play_game():
+    # welcome the player
+    print(f"""
 Hi there!
 {separator}
 I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.
 {separator}
-Enter a number: """
+Enter a number: 
+{separator}"""
 )
+    # random four-digits number generator
+    number = generate_random_number()
 
-# random number generation
-def generate_random_number():
-    number = random.sample(range(10), 4)
-    while number[0] == 0:
-        number = random.sample(range(10), 4)
-    number = ''.join(map(str, number))
-    return number
-print(generate_random_number())
+    attempts = 0
 
-# checking whether entered number is in correct format
-def check_entered_number(input):
-    if len(input) != 4 or not input.isdigit():
-        message = "Please enter a four-digit number and try again."
-    elif len(input) != len(set(input)):
-        message = "Please enter a number without duplicates and try again."
-    elif input[0] == "0":
-        message = "Please enter a number that doesn't start with 0 and try again."
-    else:
-        return True    
-    print(message, separator, sep="\n")
-    return False
+    # player's input and evaluation of player's tip
+    while True:
+        player_guess = input(">>> ")
 
-# calculation of bulls and cows based on player input 
-def bulls_and_cows_count(random_number, player_guess):
-    bulls = sum(1 for index, digit in enumerate(player_guess) if random_number[index] == digit)
-    cows = sum(1 for digit in player_guess if digit in random_number) - bulls
-    return bulls, cows
+        if check_entered_number(player_guess):
+            attempts += 1
+            if player_guess == number:
+                print(f"Correct, you've guessed the right number in {attempts} guesses!")
+                break
+            else:
+                bulls, cows = bulls_and_cows_count(number, player_guess)
+                bull_or_bulls = "bull" if bulls == 1 else "bulls"
+                cow_or_cows = "cow" if cows == 1 else "cows"
+                print(f"{bulls} {bull_or_bulls} and {cows} {cow_or_cows}")
+            print(separator)
 
-# user score rating
-def evaluate_users_score(number_of_attempts):
-    messages = {
-        range(1, 6): "Wow, excellent score!",
-        range(6, 11): "Very good score!",
-        range(11, 16): "Not so bad!",
-        range(16, 21): "Could be better!",
-        range(21, 100000): "Try again and practice."
-    }
-    
-    for attempts_range, message in messages.items():
-        if number_of_attempts in attempts_range:
-            print(message)
-            break
+    print(separator)
+    evaluate_players_score(attempts)
 
-
-
-
-
-
-
-
-
-
-def game():
-    pass
 
 if __name__ == "__main__":
-    game()
+    play_game()
